bf4ca2ce847c3a357496c28ea6886331658e2411
CalebNyachwaya <113683930+CalebNyachwaya@users.noreply.github.com>
2023-08-05 02:16:01 -0700

    Update 9-binary_tree_height.c

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -12,8 +12,11 @@ size_t binary_tree_height(const binary_tree_t *tree)
 		return (0);
 	size_t leftHeight = 0, rightHeight = 0;
 
+	/* check if left child id present and add to leftHeight */
 	leftHeight = tree->left ? 1 + binary_tree_height(tree->left) : 0;
+	/* check if right child id present and add to rightHeight */
 	rightHeight = tree->right ? 1 + binary_tree_height(tree->right) : 0;
+	/* return the greater height */
 	return ((leftHeight > rightHeight) ? leftHeight : rightHeight);
 
 	return (0);
